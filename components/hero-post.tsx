0d15fe72822018495d13c9f69b03af658dfe362c
knknknkn <k.hasegw7@gmail.com>
2023-05-10 16:35:42 +0900

    fix hero layout

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 
 type Props = {
   title: string
-  coverImage: string
   date: string
   excerpt: string
   slug: string
@@ -12,16 +11,15 @@ type Props = {
 
 const HeroPost = ({
   title,
-  coverImage,
   date,
   excerpt,
   slug,
 }: Props) => {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
+      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+        Latest Story
+      </h2>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
