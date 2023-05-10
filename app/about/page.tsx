442087a4481c55cab0593b9d206c57175c9b2f5d
knknknkn <k.hasegw7@gmail.com>
2023-05-10 17:25:41 +0900

    add skeleton screen for About

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from "react";
+import Header from "../../components/header";
+import Layout from "../../components/layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <Header children={<h2>About</h2>} />
+      {
+        <></>
+      }
+    </Layout>
+  );
+}
