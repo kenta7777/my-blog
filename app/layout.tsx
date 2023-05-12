5c0a0373b59ea8bfde4f976664acd12b2975786e
knknknkn <k.hasegw7@gmail.com>
2023-05-12 17:50:32 +0900

    set title and description for blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from 'next';
 import '../styles/index.css'
 
 type Props = {
   children: React.ReactNode
+};
+
+export const metadata: Metadata = {
+  title: 'kenta7777-blog',
+  description: 'kenta7777-blog'
 };
 
 export default function RootLayout({ children }: Props) {
